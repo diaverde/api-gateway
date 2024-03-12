22669c89e32dd1476f90db6edd722fb548ded31e
Andrés Sánchez <diaverde77@gmail.com>
2024-03-12 12:36:10 -0500

    Updated

--- Program.cs
+++ Program.cs
@@ -5,10 +5,20 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("CorsPolicy",
+        builder => builder.AllowAnyOrigin()
+            .AllowAnyMethod()
+            .AllowAnyHeader()
+            .AllowCredentials());
+});
 builder.Services.AddOcelot();
+builder.Services.AddSignalR();
 
 var app = builder.Build();
 
+app.UseWebSockets();
 app.UseOcelot().Wait();
 
 app.Run();
